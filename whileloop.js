1954c770ecdc4f1f9961771bfc965da263ec6f82
sanaalishaikh <162053739+sanaalishaikh@users.noreply.github.com>
2024-03-31 21:11:02 +0500

    Add files via upload

---
+++ whileloop.js
@@ -0,0 +1,25 @@
+// while loop
+
+idx= 1
+{
+    console.log(idx < 5);
+}
+idx= 2
+{
+    console.log(idx < 5);
+}
+idx=6
+{
+    console.log(idx < 5);
+}
+idx=10
+{
+    console.log(idx > 6);
+}
+idx=20
+{
+    console.log(idx < 10);
+}
+
+
+
